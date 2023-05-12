bad304178365ba0b25696af471098f5bf052fa8a
goosearov <goosearov@gmail.com>
2023-05-12 14:25:17 +0300

    implement FSD structure

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import './styles/index.scss'
+import {Link} from "react-router-dom";
+import {classNames} from "shered/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "./providers/router";
+
+
+
+const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>toggle</button>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <AppRouter />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/shered/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,25 @@
+import {RouteProps} from "react-router-dom";
+import {MainPage} from "pages/MainPage";
+import {AboutPage} from "pages/AboutPage";
+
+export enum AppRoutes {
+    MAIN = 'main',
+    ABOUT = 'about'
+}
+
+export const RoutPath: Record<AppRoutes, string> = {
+    [AppRoutes.MAIN]: '/',
+    [AppRoutes.ABOUT]: '/about'
+}
+
+export const routeConfig: Record<AppRoutes, RouteProps> = {
+    [AppRoutes.MAIN]: {
+        path: RoutPath.main,
+        element: <MainPage />
+    },
+
+    [AppRoutes.ABOUT]: {
+        path: RoutPath.about,
+        element: <AboutPage />
+    }
+}
