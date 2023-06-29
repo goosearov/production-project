d685e0740a6c3ee5c29226b2c4cce0e23acfdbeb
goosearov <goosearov@gmail.com>
2023-06-29 12:57:20 +0300

    add comment list

--- src/shered/ui/Code/Code.tsx
+++ src/shered/ui/Code/Code.tsx
@@ -1,5 +1,7 @@
 import { classNames } from 'shered/lib/classNames/classNames';
-import { memo, ReactNode, useCallback } from 'react';
+import {
+    memo, ReactNode, useCallback, useState,
+} from 'react';
 import { Button, ButtonTheme } from 'shered/ui/Button/Button';
 import CopyIcon from '../../assets/icons/copyIcon.svg';
 import cls from './Code.module.scss';
@@ -11,15 +13,21 @@ interface CodeProps {
 
 export const Code = memo((props: CodeProps) => {
     const { className, text } = props;
+    const [copy, setCopy] = useState(false);
 
     const onCopy = useCallback(() => {
         navigator.clipboard.writeText(text);
+        setCopy(true);
+        setTimeout(() => setCopy(false), 1000);
     }, [text]);
 
     return (
         <pre className={classNames(cls.Code, {}, [className])}>
             <Button onClick={onCopy} className={cls.copyBtn} theme={ButtonTheme.CLEAR}>
-                <CopyIcon className={cls.copyIcon} />
+                {!copy
+                    ? <CopyIcon className={cls.copyIcon} />
+
+                    : 'copied!'}
             </Button>
             <code>
                 {text}
